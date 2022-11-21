3451193fa80fa4184f47af5cef15efcbb80f9572
Mohsin <mohsin.razase@gmail.com>
2022-11-21 18:47:14 +0500

    test

--- PracticeAuth/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ PracticeAuth/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,12 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "practiceauth": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
     }
 }
